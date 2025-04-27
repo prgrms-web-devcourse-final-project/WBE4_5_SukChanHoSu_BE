c894f9c2028e68b9e95d3b0a28e918e1a0abfb22
junho767 <junho767@naver.com>
2025-04-27 23:57:58 +0900

    feat: 로그인 기능 구현
    1. 토큰 기반의 로그인 기능 구현
    2. 회원가입한 사용자의 정보를 바탕으로 accessToken, refreshToken 발급

--- backend/src/main/java/com/NBE4_5_SukChanHoSu/BE/domain/member/service/MemberService.java
+++ backend/src/main/java/com/NBE4_5_SukChanHoSu/BE/domain/member/service/MemberService.java
@@ -1,11 +1,18 @@
 package com.NBE4_5_SukChanHoSu.BE.domain.member.service;
 
+import com.NBE4_5_SukChanHoSu.BE.domain.member.dto.request.MemberLoginRequestDto;
 import com.NBE4_5_SukChanHoSu.BE.domain.member.dto.request.MemberSignUpRequestDto;
 import com.NBE4_5_SukChanHoSu.BE.domain.member.entity.Member;
 import com.NBE4_5_SukChanHoSu.BE.domain.member.entity.Role;
 import com.NBE4_5_SukChanHoSu.BE.domain.member.repository.MemberRepository;
+import com.NBE4_5_SukChanHoSu.BE.global.jwt.JwtTokenDto;
+import com.NBE4_5_SukChanHoSu.BE.global.jwt.TokenProvider;
+import com.NBE4_5_SukChanHoSu.BE.global.util.CookieUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -13,8 +20,11 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class MemberService {
+    private final AuthenticationManagerBuilder authenticationManagerBuilder;
     private final MemberRepository memberRepository;
     private final PasswordEncoder passwordEncoder;
+    private final TokenProvider tokenProvider;
+    private final CookieUtil util;
 
     public Member join(MemberSignUpRequestDto memberSignUpRequestDto) {
         Member member = Member.builder()
@@ -26,5 +36,21 @@ public class MemberService {
         memberRepository.save(member);
 
         return member;
+    }
+
+    public JwtTokenDto login(MemberLoginRequestDto requestDto) {
+        try {
+            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(requestDto.getEmail(), requestDto.getPassword());
+            Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+            JwtTokenDto jwtToken = tokenProvider.generateToken(authentication);
+
+            util.addCookie("accessToken", jwtToken.getAccessToken());
+            util.addCookie("refreshToken", jwtToken.getRefreshToken());
+
+            return jwtToken;
+        } catch (Exception e) {
+            log.error("🚨 authenticationManager.authenticate() 예외 발생!", e);
+            throw e;
+        }
     }
 }
