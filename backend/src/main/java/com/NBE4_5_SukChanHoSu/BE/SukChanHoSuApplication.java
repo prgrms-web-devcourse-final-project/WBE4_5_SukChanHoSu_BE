b9aab44de9281a29731c63091cb4a8d71557ef5f
junho767 <junho767@naver.com>
2025-04-27 16:19:21 +0900

    init commit

---
+++ backend/src/main/java/com/NBE4_5_SukChanHoSu/BE/SukChanHoSuApplication.java
@@ -0,0 +1,13 @@
+package com.NBE4_5_SukChanHoSu.BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SukChanHoSuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SukChanHoSuApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/NBE4_5_SukChanHoSu/BE/SukChanHoSuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NBE4_5_SukChanHoSu.BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SukChanHoSuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
