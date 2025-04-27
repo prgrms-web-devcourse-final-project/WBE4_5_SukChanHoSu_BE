9600903f44c29c3a9dedca02a7babba68c0248c2
junho767 <junho767@naver.com>
2025-04-27 23:53:41 +0900

    feat: 회원가입 기능 구현
    1. Jwt 라이브러리 추가
    2. Jwt-Secret 키 발급
    3. Member 도메인 추가(controller, service, repository, entity, dto)
    3. 회원가입 기능 구현
    
    # Conflicts:
    #	backend/build.gradle
    #	backend/src/main/resources/application.yml
    
    # Conflicts:
    #	backend/src/main/java/com/NBE4_5_SukChanHoSu/BE/global/dto/Empty.java
    #	backend/src/main/java/com/NBE4_5_SukChanHoSu/BE/global/dto/RsData.java

---
+++ backend/src/main/java/com/NBE4_5_SukChanHoSu/BE/domain/member/controller/MemberController.java
@@ -0,0 +1,30 @@
+package com.NBE4_5_SukChanHoSu.BE.domain.member.controller;
+
+import com.NBE4_5_SukChanHoSu.BE.domain.member.dto.MemberDto;
+import com.NBE4_5_SukChanHoSu.BE.domain.member.dto.request.MemberSignUpRequestDto;
+import com.NBE4_5_SukChanHoSu.BE.domain.member.entity.Member;
+import com.NBE4_5_SukChanHoSu.BE.domain.member.service.MemberService;
+import com.NBE4_5_SukChanHoSu.BE.global.dto.RsData;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/auth")
+public class MemberController {
+    private final MemberService memberService;
+
+    @PostMapping("/join")
+    public RsData<MemberDto> join(@RequestBody MemberSignUpRequestDto requestDto) {
+        Member member = memberService.join(requestDto);
+
+        return new RsData<>(
+                "200",
+                "회원가입이 완료되었습니다.",
+                new MemberDto(member)
+        );
+    }
+}
---
+++ backend/src/main/java/com/NBE4_5_SukChanHoSu/BE/domain/member/repository/MemberRepository.java
@@ -0,0 +1,7 @@
+package com.NBE4_5_SukChanHoSu.BE.domain.member.repository;
+
+import com.NBE4_5_SukChanHoSu.BE.domain.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+}
