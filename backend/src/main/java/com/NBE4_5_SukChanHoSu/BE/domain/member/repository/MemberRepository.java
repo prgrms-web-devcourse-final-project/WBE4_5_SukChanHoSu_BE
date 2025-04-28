9680a8882145de5953f0405c81d6d353dfa9d349
wnsgh <junho767@naver.com>
2025-04-29 01:08:01 +0900

    refactor: 회원가입, 로그인 예외처리 및 리펙토링

--- backend/src/main/java/com/NBE4_5_SukChanHoSu/BE/domain/member/repository/MemberRepository.java
+++ backend/src/main/java/com/NBE4_5_SukChanHoSu/BE/domain/member/repository/MemberRepository.java
@@ -3,6 +3,8 @@ package com.NBE4_5_SukChanHoSu.BE.domain.member.repository;
 import com.NBE4_5_SukChanHoSu.BE.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Member findByEmail(String email);
 }
--- backend/src/main/java/com/NBE4_5_SukChanHoSu/BE/domain/member/service/MemberService.java
+++ backend/src/main/java/com/NBE4_5_SukChanHoSu/BE/domain/member/service/MemberService.java
@@ -3,8 +3,10 @@ package com.NBE4_5_SukChanHoSu.BE.domain.member.service;
 import com.NBE4_5_SukChanHoSu.BE.domain.member.dto.request.MemberLoginRequestDto;
 import com.NBE4_5_SukChanHoSu.BE.domain.member.dto.request.MemberSignUpRequestDto;
 import com.NBE4_5_SukChanHoSu.BE.domain.member.entity.Member;
+import com.NBE4_5_SukChanHoSu.BE.domain.member.entity.MemberErrorCode;
 import com.NBE4_5_SukChanHoSu.BE.domain.member.entity.Role;
 import com.NBE4_5_SukChanHoSu.BE.domain.member.repository.MemberRepository;
+import com.NBE4_5_SukChanHoSu.BE.global.exception.ServiceException;
 import com.NBE4_5_SukChanHoSu.BE.global.jwt.JwtTokenDto;
 import com.NBE4_5_SukChanHoSu.BE.global.jwt.TokenProvider;
 import com.NBE4_5_SukChanHoSu.BE.global.util.CookieUtil;
@@ -20,16 +22,34 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class MemberService {
-    private final AuthenticationManagerBuilder authenticationManagerBuilder;
-    private final MemberRepository memberRepository;
-    private final PasswordEncoder passwordEncoder;
-    private final TokenProvider tokenProvider;
     private final CookieUtil util;
+    private final TokenProvider tokenProvider;
+    private final PasswordEncoder passwordEncoder;
+    private final MemberRepository memberRepository;
+    private final AuthenticationManagerBuilder authenticationManagerBuilder;
 
-    public Member join(MemberSignUpRequestDto memberSignUpRequestDto) {
+    private static final String ACCESS_TOKEN = "access_token";
+    private static final String REFRESH_TOKEN = "refresh_token";
+
+    public Member join(MemberSignUpRequestDto requestDto) {
+        if (!requestDto.getPassword().equals(requestDto.getPasswordConfirm())) {
+            throw new ServiceException(
+                    MemberErrorCode.PASSWORDS_NOT_MATCH.getCode(),
+                    MemberErrorCode.PASSWORDS_NOT_MATCH.getMessage()
+            );
+        }
+        Member checkMember = memberRepository.findByEmail(requestDto.getEmail());
+
+        if (checkMember != null) {
+            throw new ServiceException(
+                    MemberErrorCode.EMAIL_ALREADY_EXISTS.getCode(),
+                    MemberErrorCode.EMAIL_ALREADY_EXISTS.getMessage()
+            );
+        }
+
         Member member = Member.builder()
-                .email(memberSignUpRequestDto.getEmail())
-                .password(passwordEncoder.encode(memberSignUpRequestDto.getPassword()))
+                .email(requestDto.getEmail())
+                .password(passwordEncoder.encode(requestDto.getPassword()))
                 .role(Role.USER)
                 .build();
 
@@ -39,17 +59,34 @@ public class MemberService {
     }
 
     public JwtTokenDto login(MemberLoginRequestDto requestDto) {
+        Member member = memberRepository.findByEmail(requestDto.getEmail());
+
+        if (member == null) {
+            throw new ServiceException(
+                    MemberErrorCode.EMAIL_NOT_FOUND.getCode(),
+                    MemberErrorCode.EMAIL_NOT_FOUND.getMessage()
+            );
+        }
+
+        // 비밀번호 검증
+        if (!passwordEncoder.matches(requestDto.getPassword(), member.getPassword())) {
+            throw new ServiceException(
+                    MemberErrorCode.PASSWORD_INVALID.getCode(),
+                    MemberErrorCode.PASSWORD_INVALID.getMessage()
+            );
+        }
+
         try {
             UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(requestDto.getEmail(), requestDto.getPassword());
             Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
             JwtTokenDto jwtToken = tokenProvider.generateToken(authentication);
 
-            util.addCookie("accessToken", jwtToken.getAccessToken());
-            util.addCookie("refreshToken", jwtToken.getRefreshToken());
+            util.addCookie(ACCESS_TOKEN, jwtToken.getAccessToken());
+            util.addCookie(REFRESH_TOKEN, jwtToken.getRefreshToken());
 
             return jwtToken;
         } catch (Exception e) {
-            log.error("🚨 authenticationManager.authenticate() 예외 발생!", e);
+            log.error("예외 발생 -> ", e);
             throw e;
         }
     }
