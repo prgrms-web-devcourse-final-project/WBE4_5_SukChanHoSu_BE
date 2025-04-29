43c9ab66fc4e9314510967355713275a336d7990
hoechanj <jhc400573@gmail.com>
2025-04-29 11:23:02 +0900

    refactor: WebSocketconfig 설정

---
+++ backend/src/main/java/com/NBE4_5_SukChanHoSu/BE/global/WebSocketConfig.java
@@ -0,0 +1,26 @@
+package com.NBE4_5_SukChanHoSu.BE.global;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/sub"); // 구독 경로
+        config.setApplicationDestinationPrefixes("/pub"); // 발행 경로
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws-stomp") // 엔드포인트 설정
+                .setAllowedOrigins("*")
+                .withSockJS(); // SockJS 사용
+    }
+}
