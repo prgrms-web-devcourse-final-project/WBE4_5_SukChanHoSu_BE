f91f96579155fa94cf10645d1ceafa41e749c794
wonseokyoon <01088115030a@gmail.com>
2025-05-02 11:27:37 +0900

    refactor: setter 제한

--- backend/src/main/java/com/NBE4_5_SukChanHoSu/BE/domain/user/entity/UserProfile.java
+++ backend/src/main/java/com/NBE4_5_SukChanHoSu/BE/domain/user/entity/UserProfile.java
@@ -2,7 +2,6 @@ package com.NBE4_5_SukChanHoSu.BE.domain.user.entity;
 
 import com.NBE4_5_SukChanHoSu.BE.domain.likes.Matching;
 import com.NBE4_5_SukChanHoSu.BE.domain.likes.UserLikes;
-import com.NBE4_5_SukChanHoSu.BE.domain.user.entity.Gender;
 import com.NBE4_5_SukChanHoSu.BE.global.BaseTime;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
@@ -19,7 +18,6 @@ import java.util.Objects;
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
-@Setter
 @SuperBuilder
 @Table(name = "user_profile")
 public class UserProfile extends BaseTime {
@@ -27,6 +25,7 @@ public class UserProfile extends BaseTime {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userId;
 
+    @Setter
     @Column(nullable = false)
     private String nickName;
 
@@ -35,39 +34,49 @@ public class UserProfile extends BaseTime {
     private Gender gender;
 
     @Column(nullable = false)
+    @Setter
     private String profileImage;
 
     private LocalDate birthdate;
 
+    @Setter
     @ElementCollection(targetClass = Genre.class)
     @Enumerated(EnumType.STRING)
     private List<Genre> favoriteGenres;
 
     @Column(length = 100)
+    @Setter
     private String introduce;
 
     @Column(nullable = false)
+    @Setter
     private double latitude;
     @Column(nullable = false)
+    @Setter
     private double longitude;
 
     @Column(nullable = false)
+    @Setter
     private int searchRadius = 20;
 
     @OneToMany(mappedBy = "fromUser", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
     @JsonManagedReference
+    @Setter
     private List<UserLikes> likes = new ArrayList<>(); // 사용자가 누른 좋아요 목록
 
     @OneToMany(mappedBy = "toUser", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
     @JsonManagedReference
+    @Setter
     private List<UserLikes> likedBy = new ArrayList<>(); // 사용자를 좋아요한 목록
 
     @OneToMany(mappedBy = "maleUser", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
     @JsonManagedReference
+    @Setter
     private List<Matching> maleMatchings = new ArrayList<>(); // 매칭된 남자 사용자 목록
 
     @OneToMany(mappedBy = "femaleUser", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
     @JsonManagedReference
+    @Setter
     private List<Matching> femaleMatchings = new ArrayList<>(); // 매칭된 여자 사용자 목록
 
     @OneToOne
