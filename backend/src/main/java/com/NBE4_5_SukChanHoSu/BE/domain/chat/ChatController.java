36f7bad72e9ddb2b4a2962c5a476c0055014cf8c
hoechanj <jhc400573@gmail.com>
2025-05-04 01:11:47 +0900

    Feat: 1:1채팅

--- backend/src/main/java/com/NBE4_5_SukChanHoSu/BE/domain/chat/ChatController.java
+++ backend/src/main/java/com/NBE4_5_SukChanHoSu/BE/domain/chat/ChatController.java
@@ -4,14 +4,20 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
 @Controller
 @RequiredArgsConstructor
 public class ChatController {
+    private final SimpMessagingTemplate messagingTemplate;
+
+
     @MessageMapping("/chat/message")
-    @SendTo("/sub/chat/room")
-    public ChatMessage sendMessage(@Payload ChatMessage message) {
-        return message;
+    public void sendMessage(@Payload ChatMessage message) {
+        // roomId 기반으로 전송
+        messagingTemplate.convertAndSend("/sub/chat/room/" + message.getRoomId(), message);
     }
+
+
 }
