340cf7bb27d1dc258e4f07cbbf13261216168e45
wonseokyoon <01088115030a@gmail.com>
2025-05-07 11:47:49 +0900

    refactor: contextHolder UserProfileController

--- backend/src/main/java/com/NBE4_5_SukChanHoSu/BE/domain/user/entity/User.java
+++ backend/src/main/java/com/NBE4_5_SukChanHoSu/BE/domain/user/entity/User.java
@@ -1,6 +1,7 @@
 package com.NBE4_5_SukChanHoSu.BE.domain.user.entity;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -35,6 +36,6 @@ public class User {
     private String providerId;
 
     @OneToOne(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-    @JsonBackReference // 직렬화
+    @JsonIgnoreProperties({"likes", "likedBy", "maleMatchings", "femaleMatchings","user"})
     private UserProfile userProfile;
 }
