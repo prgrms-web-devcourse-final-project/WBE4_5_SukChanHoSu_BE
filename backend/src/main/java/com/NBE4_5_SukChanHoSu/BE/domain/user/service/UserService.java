20846bd3507c2b3d51d97932374a28fc58f11878
junho767 <junho767@naver.com>
2025-05-07 21:29:03 +0900

    refactor: 미사용 import 문 제거

--- backend/src/main/java/com/NBE4_5_SukChanHoSu/BE/domain/user/service/UserService.java
+++ backend/src/main/java/com/NBE4_5_SukChanHoSu/BE/domain/user/service/UserService.java
@@ -19,8 +19,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.concurrent.TimeUnit;
-
 @Slf4j
 @Service
 @RequiredArgsConstructor
