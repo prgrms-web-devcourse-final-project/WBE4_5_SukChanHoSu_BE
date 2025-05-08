734758a55ab184bc5a3cd2c4d0cbe394f3c582da
hoechanj <jhc400573@gmail.com>
2025-05-08 11:32:49 +0900

    refactor: WebSocketController 합침

--- backend/src/main/java/com/NBE4_5_SukChanHoSu/BE/domain/chat/controller/ChatController.java
+++ backend/src/main/java/com/NBE4_5_SukChanHoSu/BE/domain/chat/controller/ChatController.java
@@ -4,21 +4,19 @@ import com.NBE4_5_SukChanHoSu.BE.domain.chat.dto.ChatMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+
 
 @Controller
 @RequiredArgsConstructor
 public class ChatController {
+
     private final SimpMessagingTemplate messagingTemplate;
 
-
     @MessageMapping("/chat/message")
-    public void sendMessage(@Payload ChatMessage message, SimpMessageSendingOperations messagingTemplate) {
-        // roomId 기반으로 전송.
+    public void sendMessage(@Payload ChatMessage message) {
+        // roomId 기준으로 구독자에게 메시지 전송
         messagingTemplate.convertAndSend("/sub/chat/room/" + message.getRoomId(), message);
     }
-
-
 }
