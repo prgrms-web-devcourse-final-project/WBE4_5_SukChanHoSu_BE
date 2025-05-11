6da540bacf608634cc7d2e7787af73195903563d
taesu <taesu0608@gmail.com>
2025-05-11 22:29:00 +0900

    feat: add movieRequest

--- backend/src/main/java/com/NBE4_5_SukChanHoSu/BE/domain/movie/dto/request/MovieRequest.java
+++ backend/src/main/java/com/NBE4_5_SukChanHoSu/BE/domain/movie/dto/request/MovieRequest.java
@@ -1,4 +1,31 @@
 package com.NBE4_5_SukChanHoSu.BE.domain.movie.dto.request;
 
+import com.NBE4_5_SukChanHoSu.BE.domain.user.entity.Genre;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class MovieRequest {
+
+    @NotNull
+    private Long movieId; // = movieCd
+
+    @NotNull
+    private Genre genres; // = genres
+
+    @NotBlank
+    private String title; // = movieNm
+
+    private String releaseDate; // = openDt
+
+    private String posterImage; // = posterUrl
+
+    private String description; // = overview
+
+    private String rating; // = 미지정
+
+    private String director; // = director
 }
