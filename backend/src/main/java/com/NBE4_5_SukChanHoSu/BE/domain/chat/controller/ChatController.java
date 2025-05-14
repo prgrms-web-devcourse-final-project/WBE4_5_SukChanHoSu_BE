08cc821d94402e72026ec4ff1243346d52035729
hoechanj <jhc400573@gmail.com>
2025-05-14 11:26:18 +0900

    feat: 채팅 token

--- backend/src/main/java/com/NBE4_5_SukChanHoSu/BE/domain/chat/controller/ChatController.java
+++ backend/src/main/java/com/NBE4_5_SukChanHoSu/BE/domain/chat/controller/ChatController.java
@@ -1,12 +1,21 @@
 package com.NBE4_5_SukChanHoSu.BE.domain.chat.controller;
 
 import com.NBE4_5_SukChanHoSu.BE.domain.chat.dto.ChatMessage;
+import com.NBE4_5_SukChanHoSu.BE.domain.user.entity.UserErrorCode;
+import com.NBE4_5_SukChanHoSu.BE.global.exception.security.BadCredentialsException;
+import com.NBE4_5_SukChanHoSu.BE.global.security.PrincipalDetails;
 import com.NBE4_5_SukChanHoSu.BE.global.util.SecurityUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.server.reactive.HttpHandler;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+
+import java.security.Principal;
 
 
 @Controller
@@ -16,12 +25,29 @@ public class ChatController {
     private final SimpMessagingTemplate messagingTemplate;
 
     @MessageMapping("/chat/message")
-    public void sendMessage(@Payload ChatMessage message) {
-        // 현재 로그인한 유저의 닉네임 또는 ID 설정
-        String sender = SecurityUtil.getCurrentUser().getName();  // 또는 .getEmail(), .getId().toString()
-        message.setSender(sender);
+    public void sendMessage(@Payload ChatMessage message, Principal principal) {
 
-        // 메시지 전송
+        if (!(principal instanceof UsernamePasswordAuthenticationToken token)) {
+            throw new BadCredentialsException(
+                    UserErrorCode.USER_UNAUTHORIZED.getCode(),
+                    UserErrorCode.USER_UNAUTHORIZED.getMessage()
+            );
+        }
+
+        Object principalObj = token.getPrincipal();
+        if (!(principalObj instanceof PrincipalDetails details)) {
+            throw new BadCredentialsException(
+                    UserErrorCode.USER_UNAUTHORIZED.getCode(),
+                    UserErrorCode.USER_UNAUTHORIZED.getMessage()
+            );
+        }
+
+
+        String nickname = details.getUser().getUserProfile().getNickName(); // or getUser().getNickname() if 존재
+        message.setSender(nickname);
+
+
+         //메시지 전송
         messagingTemplate.convertAndSend("/sub/chat/room/" + message.getRoomId(), message);
     }
 }
