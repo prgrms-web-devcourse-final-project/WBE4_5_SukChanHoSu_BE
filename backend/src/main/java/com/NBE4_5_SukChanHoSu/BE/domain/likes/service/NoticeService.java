6ecd701edae4fb45466157916e6ebc60d52d75b3
wonseokyoon <01088115030a@gmail.com>
2025-05-14 12:24:10 +0900

    hotfix

--- backend/src/main/java/com/NBE4_5_SukChanHoSu/BE/domain/likes/service/NoticeService.java
+++ backend/src/main/java/com/NBE4_5_SukChanHoSu/BE/domain/likes/service/NoticeService.java
@@ -93,7 +93,7 @@ public class NoticeService {
     private void processStream(String streamName,String lastId, Consumer<MapRecord<String,Object,Object>> processEvent) {
         StreamReadOptions options = StreamReadOptions.empty().count(1);
         // 마지막으로 읽은 Id 부터 읽음
-        StreamOffset<String> offset = StreamOffset.create(LIKE_STREAM, ReadOffset.from(lastLikeId));
+        StreamOffset<String> offset = StreamOffset.create(streamName, ReadOffset.from(lastId));
         List<MapRecord<String, Object, Object>> records = redisTemplate.opsForStream().read(options, offset);
 
 //        // 블로킹 읽기 옵션 설정 (10초 대기)
