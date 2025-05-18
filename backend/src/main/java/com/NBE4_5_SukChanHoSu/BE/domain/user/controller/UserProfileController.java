471120028b49209476888e5bff585c222078e58e
junho1131 <45115780+junho1131@users.noreply.github.com>
2025-05-18 21:28:20 +0900

    Feat/manyprofileimage add/#141 (#144)
    * feat/admin-delete-review & filtering
    
    * feat:manyprofileimage-add
    
    * chore:remove overlap
    
    * feat: add update profanity
    
    * feat:userprofile id link user.id

--- backend/src/main/java/com/NBE4_5_SukChanHoSu/BE/domain/user/controller/UserProfileController.java
+++ backend/src/main/java/com/NBE4_5_SukChanHoSu/BE/domain/user/controller/UserProfileController.java
@@ -27,6 +27,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/profile")
@@ -40,18 +41,19 @@ public class UserProfileController {
     @Operation(summary = "프로필 등록", description = "회원가입 후 최초 프로필 등록")
     @PostMapping(consumes = "multipart/form-data")
     @ResponseStatus(HttpStatus.CREATED)
-    public RsData<ProfileResponse> createProfile(@RequestPart("dto")  ProfileRequest dto,
-                                                 @RequestPart(value = "profileImage", required = false) MultipartFile profileImageFile) throws IOException {
+    public RsData<ProfileResponse> createProfile(@ModelAttribute  ProfileRequest dto,
+                                                 @RequestPart(value = "profileImages", required = false) List<MultipartFile> profileImages) throws IOException {
 
-        ProfileResponse response = userProfileService.createProfile(SecurityUtil.getCurrentUserId(), dto,profileImageFile);
+        ProfileResponse response = userProfileService.createProfile(SecurityUtil.getCurrentUserId(), dto,profileImages);
         return new RsData<>("201", "프로필 등록 완료", response);
     }
 
     @Operation(summary = "프로필 수정", description = "닉네임, 성별, 위치 등 프로필 정보 수정")
     @PutMapping(consumes = "multipart/form-data")
-    public RsData<ProfileResponse> updateProfile(@RequestPart("dto") ProfileUpdateRequest dto,
-                                                 @RequestPart(value = "profileImage", required = false) MultipartFile profileImageFile) throws IOException {
-        ProfileResponse response = userProfileService.updateProfile(SecurityUtil.getCurrentUserId(), dto,profileImageFile);
+    public RsData<ProfileResponse> updateProfile(@ModelAttribute ProfileUpdateRequest dto,
+                                                 @RequestPart(value = "profileImages", required = false) List<MultipartFile> profileImages,
+                                                 @RequestParam(value = "imagesToDelete", required = false) List<String> imagesToDelete) throws IOException {
+        ProfileResponse response = userProfileService.updateProfile(SecurityUtil.getCurrentUserId(), dto,profileImages,imagesToDelete);
         return new RsData<>("200", "프로필 수정 완료", response);
     }
 
